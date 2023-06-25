2e41ef888c724788840b67ee210f55f4a6e029b3
ahmedherhesh <ahmedherhesh3@gmail.com>
2023-06-25 19:09:19 +0300

    analysis

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,15 +8,25 @@
     @include('includes.sidebar')
     <div class="content">
         <div class="progress-section d-flex gap-4 justify-content-center">
-            <div class="ctm-progress-bar p-2 bg-light" count='{{ $items->count() }}'>المنتجات</div>
+            <div class="ctm-progress-bar p-2 bg-light" count='{{ $items_count }}'>المنتجات</div>
             <div class="ctm-progress-bar p-2 bg-light" count='{{ $categories->count() }}'>الأقسام</div>
-            <div class="ctm-progress-bar p-2 bg-light" count='0'>التسليمات</div>
-            <div class="ctm-progress-bar p-2 bg-light" count='0'>المرتجعات</div>
-            <div class="ctm-progress-bar p-2 bg-light" count='0'>التقارير</div>
+            <div class="ctm-progress-bar p-2 bg-light" count='{{ $deliveries_count }}'>التسليمات</div>
+            <div class="ctm-progress-bar p-2 bg-light" count='{{ $returned_items_count }}'>المرتجعات</div>
+            <div class="ctm-progress-bar p-2 bg-light" count='{{ $deliveries_count + $returned_items_count }}'>التقارير
+            </div>
+        </div>
+        <div class="d-flex justify-content-center align-items-center mt-5">
+            @forelse ($items as $item)
+                <x-item :item="$item" />
+            @empty
+                @include('includes.empty')
+            @endforelse
+            @include('includes.modals.delivery-item-modal')
+        </div>
+        <div class="mt-5 d-flex justify-content-center">
+            {{ $items->links('vendor.pagination.bootstrap-4') }}
         </div>
     </div>
-    {{-- {{ view()->make('items.items', ['items' => $items]) }} --}}
-    {{-- @include('items.items',['items' => $items]) --}}
 @endsection
 
 @section('js')
